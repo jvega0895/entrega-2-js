b530d5685adf090ed22d0cebea63c8b54fae848f
Juan Vega <juan.vega.0895@gmail.com>
2022-11-18 17:34:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const pizzas = [
+  {
+    nombre: "Pizza de Muzzarella",
+    id: 1,
+    ingredientes: [" Sus ingredientes son", "muzzarella","oregano","aceitunas"],
+    precio: 750
+},
+{
+  nombre: "Pizza de Cebolla",
+  id:2,
+  ingredientes: [ " Sus ingredientes son", "muzzarella", "cebolla"],
+  precio:800
+},
+{
+  nombre: "Pizza de 4 quesos",
+  id:3,
+  ingredientes: [ " Sus ingredientes son","queso Fontina", "queso Muzzarella", "queso Azul", "queso Parmesano"],
+  precio:1100
+},
+{
+  nombre:"Pizza  de Anchoas",
+  id:4,
+  ingredientes: [ " Sus ingredientes son","Muzzarella", "tomate", "morron", "anchoas"],
+  precio:1150
+},
+{
+  nombre: "Pizza de Panceta",
+  id:5,
+  ingredientes: ["Sus ingredientes son:","queso", "panceta", "tomate"],
+  precio: 1320
+},
+{
+  nombre: "Pizza Bomba",
+  id: 6,
+  ingredientes: ["Sus ingredientes son:", "Muzzarella","Cebolla","Jamon","Morron","Panceta","Palmito","Anchoas", "Aceitunas"],
+  precio: 2000
+}
+
+ ]
+ const pizzasContainer = document.getElementById("pizzas-container")
+ const inputNumber = document.getElementById("idPizza")
+ 
+ const error = (input, mensaje) => {
+ const inputError = input.value
+ const errorMensaje = inputError.querySelector("small")
+ errorMensaje.textContent = mensaje;
+ }
+ 
+ const getPizzaHtml = (pizzas) => {
+  if (inputNumber.value === pizzas.id) {
+    return `<h2>${pizzas.nombre}</h2>
+            <h3>${pizzas.precio}</h3>`
+  } else{
+    error(inputNumber, "el id de su pizza no es valido")
+  }
+ } 
+
+ const pizzasHtml = pizzas.map((pizzas)=>getPizzaHtml(pizzas)).join("")
+
+ pizzasContainer.innerHTML = pizzasHtml
+
+ pizzasContainer.addEventListener("submit", (e)=>{
+  e.preventDefault();
+ })
+
+ 
